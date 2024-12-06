a5a39a6d8fff2d18099861996d51d389f8b6c5cb
SachinNavalagund <sachinnavalagund1@gmail.com>
2024-12-06 18:25:34 +0530

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import NonDashboardNavbar from '@/components/NonDashboardNavbar';
+import Landing from './(nondashboard)/landing/page';
+import Footer from '@/components/Footer';
+
 const HomePage = () => {
-  return <div className="">Home page</div>;
+  return (
+    <div className="nondashboard-layout">
+      <NonDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="footer">
+      <p> &copy; {currentYear} LMS. All Rights Reserved </p>
+      <div className="footer__links">
+        {['About', 'Privacy Policy', 'Licensing', 'Contact'].map((item) => (
+          <Link
+            href={`/${item.toLowerCase().replace(' ', '-')}`}
+            key={item}
+            className="footer__link"
+          >
+            {item}
+          </Link>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
